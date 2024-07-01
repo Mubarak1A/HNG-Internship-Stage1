d00fee7b934db11bd39b6ac8c7dd4096cfa3543b
Mubarak <desinamubarak123@gmail.com>
2024-07-01 23:25:32 +0100

    update: get all data from weatherapi.com and finish up api/hello  route

--- index.js
+++ index.js
@@ -5,8 +5,26 @@ const app = express()
 
 require('dotenv').config()
 
-app.get('/api/hello', (req, res) => {
-    res.send(req.ip)
+app.get('/api/hello', async (req, res) => {
+    const clientName = req.query.user_name || 'User'
+
+    let clientIp = (req.ip != '::ffff:127.0.0.1') ? req.ip : '41.203.78.171'
+
+    const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${process.env.API_KEY}&q=${clientIp}`
+
+    const response = await fetch(apiUrl, {
+        mode: 'cors'
+    });
+   const data = await response.json();
+
+   const location = data.location.name;
+   const temp = data.current.temp_c
+
+   res.json({
+    "client_ip": clientIp,
+    "location": location,
+    "greeting": `Hello, ${clientName}! the temperature is ${temp} degree celcius in ${location}`
+})
 })
 
 
